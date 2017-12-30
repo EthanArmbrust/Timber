d2a014e6edf7f048980726b8c024f3f8c57cf1da
ethanarm <ethanarm@buffalo.edu>
2017-12-30 12:25:39 -0500

    Playlist correctly updates now after removing a song.

--- app/src/main/java/com/naman14/timber/adapters/PlayingQueueAdapter.java
+++ app/src/main/java/com/naman14/timber/adapters/PlayingQueueAdapter.java
@@ -107,7 +107,7 @@ public class PlayingQueueAdapter extends RecyclerView.Adapter<PlayingQueueAdapte
                                 MusicPlayer.removeTrackAtPosition(getSongAt(position).id, position);
                                 removeSongAt(position);
                                 notifyItemRemoved(position);
-                                notifyDataSetChanged();
+                                notifyItemRangeChanged(position, getItemCount());
                                 break;
                             case R.id.popup_song_play:
                                 MusicPlayer.playAll(mContext, getSongIds(), position, -1, TimberUtils.IdType.NA, false);
